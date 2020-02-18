894fbbf76afa22c6e2057f0d9b94c647a17e1d25
Chen Hui Jing <kakyou_tensai@yahoo.com>
2020-02-18 17:31:05 +0800

    Remove sample code

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 (function() {
-  let old = console.log;
   const LOG = document.getElementById('log');
   console.log = function(message) {
     if (typeof message == 'object') {
@@ -9,22 +8,3 @@
     }
   }
 })();
-
-function buildList(list) {
-  let result = [];
-  for (var i = 0; i < list.length; i++) {
-    let item = 'item' + i;
-    result.push(function() { console.log(item + ' ' + list[i]) });
-  }
-  return result;
-}
-
-function testList() {
-  let fnlist = buildList([1, 2, 3]);
-  // Using j only to help prevent confusion -- could use i.
-  for (var j = 0; j < fnlist.length; j++) {
-    fnlist[j]();
-  }
-}
-
-testList() //logs "item2 undefined" 3 times
